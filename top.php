f3329086aab850f4ba5e832e1fd40bd25fac1605
ROllerozxa <rollerozxa@voxelmanip.se>
2023-06-23 23:07:32 +0200

    Add pagination on popular and top pages

--- top.php
+++ top.php
@@ -1,6 +1,15 @@
 <?php
 require('lib/common.php');
 
-$levels = query("SELECT $userfields l.id id,l.title title,l.likes likes FROM levels l JOIN users u ON l.author = u.id WHERE l.visibility = 0 ORDER BY l.likes DESC, l.id DESC LIMIT $lpp");
+$page = (isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0 ? $_GET['page'] : 1);
 
-echo twigloader()->render('top.twig', ['levels' => fetchArray($levels)]);
+$limit = sprintf("LIMIT %s,%s", (($page - 1) * $lpp), $lpp);
+$levels = query("SELECT $userfields l.id id,l.title title FROM levels l
+		JOIN users u ON l.author = u.id WHERE l.visibility = 0 ORDER BY l.likes DESC, l.id DESC $limit");
+$count = result("SELECT COUNT(*) FROM levels WHERE visibility = 0");
+
+echo twigloader()->render('top.twig', [
+	'levels' => $levels,
+	'page' => $page,
+	'level_count' => $count
+]);
