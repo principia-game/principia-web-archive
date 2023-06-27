01d37121ca4567f7024de20da503fe2bdada6858
ROllerozxa <rollerozxa@voxelmanip.se>
2023-06-27 16:49:41 +0200

    Add report page

---
+++ report.php
@@ -0,0 +1,19 @@
+<?php
+require('lib/common.php');
+
+if (isset($_POST['kuk'])) {
+	$text = $_POST['kuk'] ?? null;
+	$url = $_POST['helvete'] ?? null;
+
+	query("INSERT INTO reports (reporttext, url, ip) VALUES (?,?,?)",
+		[$text, $url, $ipaddr]);
+
+	$hasBeenSent = true;
+}
+
+$url = $_GET['url'] ?? null;
+
+echo twigloader()->render('report.twig', [
+	'url' => $url,
+	'has_been_sent' => $hasBeenSent ?? false
+]);
