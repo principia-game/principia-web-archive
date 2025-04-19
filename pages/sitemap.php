22291db34e2de30e2f2be2a0f716b305dfa4fd24
ROllerozxa <rollerozxa@voxelmanip.se>
2025-04-19 17:30:33 +0200

    Upstream paginated sitemap

--- pages/sitemap.php
+++ pages/sitemap.php
@@ -1,18 +1,13 @@
 <?php
 require('../principia-web/lib/sitemap.php');
 
+$page = $_GET['page'] ?? 1;
+
 $sitemap = new Sitemap('https://archive.principia-web.se/');
 
-$sitemap->add('');
-
-$levels = query("SELECT id FROM levels WHERE visibility = 0");
+$levels = query("SELECT id FROM levels WHERE visibility = 0".paginate($page, 5000));
 while ($level = $levels->fetch()) {
 	$sitemap->add('level/'.$level['id']);
-}
-
-$users = query("SELECT id FROM users ORDER BY id");
-while ($user = $users->fetch()) {
-	$sitemap->add('user/'.$user['id']);
 }
 
 $sitemap->output();
